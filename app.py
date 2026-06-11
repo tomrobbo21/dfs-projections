d68f5190389ff3087850eab256960df7958cbd47
tomrobbo21 <tjrobson21@gmail.com>
2026-06-11 17:31:54 +1000

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ NAME_CORRECTIONS = {
     'Jack Carroll':'Jack_Carroll1','Joshua Draper':'Josh_Draper',
     'Nicholas Madden':'Nick_Madden','Thomas Edwards':'Tom_Edwards',
     'William Hayes':'Will_Hayes1','William Edwards':'Will_Edwards',
-    ''Hugo Hall-Kahan': 'Hugo_Hall-Kahan',
+    'Hugo Hall-Kahan': 'Hugo_Hall-Kahan',
 }
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
